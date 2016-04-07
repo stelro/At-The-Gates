811bcb311de77303913d370ed2401862b7b79de1
stelro <stelmach.ro@gmail.com>
2016-04-07 20:44:27 +0300

    dynamic stack

--- lib/dstack.h
+++ lib/dstack.h
@@ -1,8 +1,41 @@
-/**
- * Dynamic Stack 
- * Stelmach Ro (c) 2015-2016
- * stelmach.ro_at_gmail.com
- **/
+/*
+ *  lib/dstack.h - Dynamic template Stack
+ *
+ *  methods to use
+ *
+ *  Stack() //main constructor
+ *  Stack(Stack s) //copy constructor
+ *  s1 = s2 //overloaded assigment operator
+ *  push() //item T , if push back was successful returns true,
+ *  othervise false
+ *  pop() // returns the top item from the stuck and delete it
+ *  	if error occurred throws OutOfBoundException
+ *  IsEmpty // returns true if stack is empty, false othervise
+ *  top() // returns the top item from the stack,
+ *  	if error occurred throws OutOfBoundException
+ *  back() // returns the back of the stack, (first item)
+ *  	if error occurred throws OutOfBoundException
+ *  size() //returns the size of the stack
+ *
+ *
+ *
+ *
+ *  This file is a part of At-The-Gates SDL2 Game
+ *  Copyright (C) 2016  Stelmach Rostislav
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef DSTACK_H
 #define DSTACK_H
